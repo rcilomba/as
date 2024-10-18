f24c8456ab3c6de243be0ecb592d30e8e989c80c
rcilomba <rcilomba@yahoo.com>
2024-10-18 02:57:14 +0200

    changs Slider.js

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,4 +1,4 @@
-import { Pagination, Navigation } from "swiper/modules";
+import { Pagination } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
