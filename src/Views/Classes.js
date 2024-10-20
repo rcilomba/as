bbf46a1ccb341d72d899398da59b74dce122d2c1
rcilomba <rcilomba@yahoo.com>
2024-10-20 04:11:51 +0200

    teams update plus tiny classes schedule

--- src/Views/Classes.js
+++ src/Views/Classes.js
@@ -1,7 +1,19 @@
 import React from "react";
+import schedule from "../assets/schedule.png";
 
 export const Classes = () => {
-  return <div>Classes</div>;
+  return (
+    <div className="flex flex-col items-center">
+      <h1 className="text-2xl font-bold mb-4">Classes</h1>
+      <div className="flex justify-center">
+        <img
+          src={schedule}
+          alt="Class Schedule"
+          className="max-w-full h-auto"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Classes;
--- src/Views/TheTeam.js
+++ src/Views/TheTeam.js
@@ -1,27 +1,30 @@
 import React from "react";
-import founder1 from "../assets/TheTeam/andreas.webp";
-import founder2 from "../assets/TheTeam/majdi.webp";
-import founder3 from "../assets/TheTeam/alex.webp";
-import team1 from "../assets/TheTeam/jeppe.webp";
-import team2 from "../assets/TheTeam/oliver.webp";
+import andreas from "../assets/TheTeam/andreas.webp";
+import majdi from "../assets/TheTeam/majdi.webp";
+import alexander from "../assets/TheTeam/alex.webp";
+import jeppe from "../assets/TheTeam/jeppe.webp";
+import oliver from "../assets/TheTeam/oliver.webp";
+import alan from "../assets/TheTeam/alan.webp";
+import bianca from "../assets/TheTeam/bianca.webp";
+import { FaInstagram } from "react-icons/fa"; // Importera Instagram-ikonen
 
 const founders = [
   {
     founderName: "Andreas 'The General' Michael",
     founderTitle: "Founder/Head Coach",
-    founderImage: founder1,
+    founderImage: andreas,
     founderLink: "https://www.instagram.com/andreasthegeneral/",
   },
   {
     founderName: "Majdi Shammas",
     founderTitle: "Founder/Manager",
-    founderImage: founder2,
+    founderImage: majdi,
     founderLink: "https://www.instagram.com/majdishammas/",
   },
   {
     founderName: 'Alexander "The Manuler" Gustafsson',
     founderTitle: "Founder/Fighter/UFC Hall of Famer/Coach",
-    founderImage: founder3,
+    founderImage: alexander,
     founderLink: "https://www.instagram.com/alexthemauler/",
   },
 ];
@@ -30,14 +33,145 @@ const teams = [
   {
     teamName: "Jeppe Hoffman",
     teamTitle: "Front Desk/Coach/PT (Thaiboxing)",
-    teamImage: team1,
+    teamImage: jeppe,
     teamLink: "https://www.instagram.com/jeppetraining/",
   },
-
   {
     teamName: "Oliver Roque",
     teamTitle: "Front Desk/Coach/Pt (Thai)",
-    teamImage: team2,
+    teamImage: oliver,
+    teamLink: "https://www.instagram.com/olive.rroque/",
+  },
+  {
+    teamName: 'Alan Finfo" Nascimento',
+    teamTitle: "Coach (BJJ)",
+    teamImage: alan,
+    teamLink: "https://www.instagram.com/alanfinfoujj/",
+  },
+  {
+    teamName: "Bianca Antman",
+    teamTitle: "Kids coach/Coach/PT (BJJ/MMA)",
+    teamImage: bianca,
+    teamLink: "https://www.instagram.com/biancaantman/",
+  },
+  {
+    teamName: "Jeppe Hoffman",
+    teamTitle: "Front Desk/Coach/PT (Thaiboxing)",
+    teamImage: jeppe,
+    teamLink: "https://www.instagram.com/jeppetraining/",
+  },
+  {
+    teamName: "Oliver Roque",
+    teamTitle: "Front Desk/Coach/Pt (Thai)",
+    teamImage: oliver,
+    teamLink: "https://www.instagram.com/olive.rroque/",
+  },
+  {
+    teamName: "Jeppe Hoffman",
+    teamTitle: "Front Desk/Coach/PT (Thaiboxing)",
+    teamImage: jeppe,
+    teamLink: "https://www.instagram.com/jeppetraining/",
+  },
+  {
+    teamName: "Oliver Roque",
+    teamTitle: "Front Desk/Coach/Pt (Thai)",
+    teamImage: oliver,
+    teamLink: "https://www.instagram.com/olive.rroque/",
+  },
+  {
+    teamName: "Jeppe Hoffman",
+    teamTitle: "Front Desk/Coach/PT (Thaiboxing)",
+    teamImage: jeppe,
+    teamLink: "https://www.instagram.com/jeppetraining/",
+  },
+  {
+    teamName: "Oliver Roque",
+    teamTitle: "Front Desk/Coach/Pt (Thai)",
+    teamImage: oliver,
+    teamLink: "https://www.instagram.com/olive.rroque/",
+  },
+  {
+    teamName: "Jeppe Hoffman",
+    teamTitle: "Front Desk/Coach/PT (Thaiboxing)",
+    teamImage: jeppe,
+    teamLink: "https://www.instagram.com/jeppetraining/",
+  },
+  {
+    teamName: "Oliver Roque",
+    teamTitle: "Front Desk/Coach/Pt (Thai)",
+    teamImage: oliver,
+    teamLink: "https://www.instagram.com/olive.rroque/",
+  },
+  {
+    teamName: "Jeppe Hoffman",
+    teamTitle: "Front Desk/Coach/PT (Thaiboxing)",
+    teamImage: jeppe,
+    teamLink: "https://www.instagram.com/jeppetraining/",
+  },
+  {
+    teamName: "Oliver Roque",
+    teamTitle: "Front Desk/Coach/Pt (Thai)",
+    teamImage: oliver,
+    teamLink: "https://www.instagram.com/olive.rroque/",
+  },
+  {
+    teamName: "Jeppe Hoffman",
+    teamTitle: "Front Desk/Coach/PT (Thaiboxing)",
+    teamImage: jeppe,
+    teamLink: "https://www.instagram.com/jeppetraining/",
+  },
+  {
+    teamName: "Oliver Roque",
+    teamTitle: "Front Desk/Coach/Pt (Thai)",
+    teamImage: oliver,
+    teamLink: "https://www.instagram.com/olive.rroque/",
+  },
+  {
+    teamName: "Jeppe Hoffman",
+    teamTitle: "Front Desk/Coach/PT (Thaiboxing)",
+    teamImage: jeppe,
+    teamLink: "https://www.instagram.com/jeppetraining/",
+  },
+  {
+    teamName: "Oliver Roque",
+    teamTitle: "Front Desk/Coach/Pt (Thai)",
+    teamImage: oliver,
+    teamLink: "https://www.instagram.com/olive.rroque/",
+  },
+  {
+    teamName: "Jeppe Hoffman",
+    teamTitle: "Front Desk/Coach/PT (Thaiboxing)",
+    teamImage: jeppe,
+    teamLink: "https://www.instagram.com/jeppetraining/",
+  },
+  {
+    teamName: "Oliver Roque",
+    teamTitle: "Front Desk/Coach/Pt (Thai)",
+    teamImage: oliver,
+    teamLink: "https://www.instagram.com/olive.rroque/",
+  },
+  {
+    teamName: "Jeppe Hoffman",
+    teamTitle: "Front Desk/Coach/PT (Thaiboxing)",
+    teamImage: jeppe,
+    teamLink: "https://www.instagram.com/jeppetraining/",
+  },
+  {
+    teamName: "Oliver Roque",
+    teamTitle: "Front Desk/Coach/Pt (Thai)",
+    teamImage: oliver,
+    teamLink: "https://www.instagram.com/olive.rroque/",
+  },
+  {
+    teamName: "Jeppe Hoffman",
+    teamTitle: "Front Desk/Coach/PT (Thaiboxing)",
+    teamImage: jeppe,
+    teamLink: "https://www.instagram.com/jeppetraining/",
+  },
+  {
+    teamName: "Oliver Roque",
+    teamTitle: "Front Desk/Coach/Pt (Thai)",
+    teamImage: oliver,
     teamLink: "https://www.instagram.com/olive.rroque/",
   },
 ];
@@ -65,12 +199,18 @@ export const TheTeam = () => {
                   alt={founder.founderName}
                   className="w-full h-48 object-cover"
                 />
-                {/* text under bild, dold i desktop */}
                 <div className="mt-2 text-center md:hidden">
-                  {" "}
-                  {/* hidden in desktop */}
+                  {/* text under bild, dold i desktop */}
                   <h3 className="text-lg font-bold">{founder.founderName}</h3>
                   <p className="text-sm">{founder.founderTitle}</p>
+                  <a
+                    href={founder.founderLink}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="mt-1 flex justify-center"
+                  >
+                    <FaInstagram className="text-blue-500" />
+                  </a>
                 </div>
                 <div className="absolute inset-0 bg-blue-500 opacity-0 group-hover:opacity-75 flex flex-col items-center justify-center text-white transition-opacity duration-300">
                   <h3 className="text-lg font-bold">{founder.founderName}</h3>
@@ -81,10 +221,10 @@ export const TheTeam = () => {
           ))}
         </div>
       </section>
-      {/* the team */}
+      {/* The Team */}
       <section>
         <h1>The Team</h1>
-        <div className="max-w-3xl grid grid-cols-1 md:grid-cols-4 gap-4 p-4 mx-auto">
+        <div className="max-w-3xl grid grid-cols-2 md:grid-cols-4 gap-4 p-4 mx-auto">
           {teams.map((team, index) => (
             <div
               key={index}
@@ -101,12 +241,18 @@ export const TheTeam = () => {
                   alt={team.teamName}
                   className="w-full h-48 object-cover"
                 />
-                {/* text under bild, dold i desktop */}
                 <div className="mt-2 text-center md:hidden">
-                  {" "}
-                  {/* hidden in desktop */}
+                  {/* text under bild, dold i desktop */}
                   <h3 className="text-lg font-bold">{team.teamName}</h3>
                   <p className="text-sm">{team.teamTitle}</p>
+                  <a
+                    href={team.teamLink}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="mt-1 flex justify-center"
+                  >
+                    <FaInstagram className="text-blue-500" />
+                  </a>
                 </div>
                 <div className="absolute inset-0 bg-blue-500 opacity-0 group-hover:opacity-75 flex flex-col items-center justify-center text-white transition-opacity duration-300">
                   <h3 className="text-lg font-bold">{team.teamName}</h3>
