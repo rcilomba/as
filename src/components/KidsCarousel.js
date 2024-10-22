9f9a42f876b6525a1c3f58c627b452396ecdde1a
rcilomba <rcilomba@yahoo.com>
2024-10-22 22:40:50 +0200

    swipe update

--- src/components/KidsCarousel.js
+++ src/components/KidsCarousel.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { useSwipeable } from "react-swipeable";
 import img1 from "../assets/kids/img1.jpeg";
 import img2 from "../assets/kids/img2.jpeg";
 import img3 from "../assets/kids/img3.jpeg";
@@ -20,13 +21,30 @@ const KidsCarousel = () => {
     return () => clearInterval(intervalId); // Cleanup the interval on component unmount
   }, []);
 
+  const handlers = useSwipeable({
+    onSwipedLeft: () => {
+      setCurrentIndex((prevIndex) =>
+        prevIndex === images.length - 1 ? 0 : prevIndex + 1
+      );
+    },
+    onSwipedRight: () => {
+      setCurrentIndex((prevIndex) =>
+        prevIndex === 0 ? images.length - 1 : prevIndex - 1
+      );
+    },
+    preventDefaultTouchmoveEvent: true,
+    trackMouse: true, // Enable mouse tracking for desktop use
+  });
+
   return (
     <section className="relative w-full max-w-7xl mx-auto">
-      <div className="overflow-hidden">
+      <div className="overflow-hidden" {...handlers}>
+        {" "}
+        {/* Attach swipe handlers here */}
         <img
           src={images[currentIndex]}
-          alt={`Kids class ${currentIndex + 1}`} // Updated alt text
-          className="w-3/5 h-auto object-cover m-auto" // Adjust height to a fixed value
+          alt={`Kids class ${currentIndex + 1}`}
+          className="w-3/5 h-auto object-cover m-auto"
         />
       </div>
       <div className="flex justify-between mt-4"></div>
